3b4863bfc03aafc7dd8ded81b381cf5fc1f3a117
YBlas <make202615@gmail.com>
2026-02-02 20:46:28 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import routerPersonas from "./routes";
 import dotenv from "dotenv";
-import { connectMongoDB } from "./mongo";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 app.use("/", routerPersonas);
 
